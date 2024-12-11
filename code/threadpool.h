03759999a59d93e84132f2e20700f5fb3338acf1
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-12-11 15:59:34 +0100

    wip: creation sous-classe Worker and TaskQueue

--- code/threadpool.h
+++ code/threadpool.h
@@ -1,6 +1,7 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <atomic>
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -9,6 +10,8 @@
 #include <pcosynchro/pcologger.h>
 #include <pcosynchro/pcothread.h>
 #include <pcosynchro/pcohoaremonitor.h>
+#include <pcosynchro/pcomutex.h>
+#include <pcosynchro/pcoconditionvariable.h>
 
 class Runnable {
 public:
@@ -18,18 +21,66 @@ public:
     virtual std::string id() = 0;
 };
 
+
+// sous-classe qui encapsule la file d'attente des tâches
+class TaskQueue {
+public:
+    TaskQueue(size_t maxSize) : maxSize(maxSize) {}
+
+    bool push(std::unique_ptr<Runnable> task) {
+        if (queue.size() >= maxSize) {
+            return false; // File d'attente pleine.
+        }
+        queue.push(std::move(task));
+        return true;
+    }
+
+
+private:
+    std::queue<std::unique_ptr<Runnable>> queue;
+    size_t maxSize;
+};
+
+
+// sous-classe qui gère un thread individuel du pool
+class Worker {
+public:
+    Worker(TaskQueue& taskQueue, std::atomic<size_t>& activeCount)
+        : taskQueue(taskQueue),  activeCount(activeCount), thread(&Worker::run, this) {}
+
+    ~Worker() {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+
+private:
+    void run() {
+       
+    }
+
+    TaskQueue& taskQueue;
+    std::atomic<size_t>& activeCount;
+    std::thread thread;
+};
+
+
 class ThreadPool {
 public:
     ThreadPool(int maxThreadCount, int maxNbWaiting, std::chrono::milliseconds idleTimeout)
-        : maxThreadCount(maxThreadCount), maxNbWaiting(maxNbWaiting), idleTimeout(idleTimeout) {
+        : maxThreadCount(maxThreadCount), maxNbWaiting(maxNbWaiting), idleTimeout(idleTimeout),taskQueue(maxNbWaiting), activeThreads(0) {
+            for (int i = 0; i < maxThreadCount; ++i) {
+            workers.emplace_back(std::make_unique<Worker>(taskQueue, activeThreads));
+        }
     }
 
     ~ThreadPool() {
         // TODO : End smoothly
+        
     }
 
     /*
-     * Start a runnable. If a thread in the pool is avaible, assign the
+     * Start a runnable. If a thread in the pool is available, assign the
      * runnable to it. If no thread is available but the pool can grow, create a new
      * pool thread and assign the runnable to it. If no thread is available and the
      * pool is at max capacity and there are less than maxNbWaiting threads waiting,
@@ -38,7 +89,7 @@ public:
      */
     bool start(std::unique_ptr<Runnable> runnable) {
         // TODO
-        return false;
+        return taskQueue.push(std::move(runnable));
     }
 
     /* Returns the number of currently running threads. They do not need to be executing a task,
@@ -46,7 +97,6 @@ public:
      */
     size_t currentNbThreads() {
         // TODO
-        return 0;
     }
 
 private:
@@ -54,6 +104,10 @@ private:
     size_t maxThreadCount;
     size_t maxNbWaiting;
     std::chrono::milliseconds idleTimeout;
+
+    TaskQueue taskQueue;
+    std::atomic<size_t> activeThreads;
+    std::vector<std::unique_ptr<Worker>> workers;
 };
 
 #endif // THREADPOOL_H
